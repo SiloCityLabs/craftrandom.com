dda759b23a5047818c030a1b099ae6282b29084e
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-12-09 19:12:08 +0000

    Built out the give command for loadout

--- helpers/fetchItems.tsx
+++ helpers/fetchItems.tsx
@@ -1,11 +1,17 @@
 import dataList from "../json/items.json";
 import { randomListItem } from "./randomListItem";
+import { MinecraftItem } from "../types/Minecraft";
 
-export function fetchItems(amount: number = 36): string[] {
-    const items: string[] = [];
+export function fetchItems(amount: number = 36): MinecraftItem[] {
+    const items: MinecraftItem[] = [];
 
     for (let i = 0; i < amount; i++) {
-        items.push(randomListItem(dataList));
+        let newItem;
+        do {
+            newItem = randomListItem(dataList);
+        } while (items.includes(newItem)); // Check if item already exists
+
+        items.push(newItem);
     }
 
     return items;
