c33f0acbda09a96e888094fb0a6f8efec7bb7f4a
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-12-10 20:33:44 +0000

    Remoeved the bootstrap and switched to pure flexbox

--- components/Inventory.tsx
+++ components/Inventory.tsx
@@ -1,55 +1,43 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 //Css
 import '../public/styles/components/Inventory.css';
 
 function Inventory() {
     return (
-        <Container className="inventory-container" style={{ width: '500px' }}>
-            {/* Armor and Off-hand Slots */}
-            <Row className="top-row" xs={9}> {/* Use xs={9} here */}
-                <Col xs={2} className='armor-slots-col'> {/* Adjust back to xs={2} */}
-                    <Row xs={1} className="armor-slots">
-                        <Col>{/* Helmet */}</Col>
-                    </Row>
-                    <Row xs={1} className="armor-slots">
-                        <Col>{/* Chestplate */}</Col>
-                    </Row>
-                    <Row xs={1} className="armor-slots">
-                        <Col>{/* Leggings */}</Col>
-                    </Row>
-                    <Row xs={1} className="armor-slots">
-                        <Col>{/* Boots */}</Col>
-                    </Row>
-                </Col>
-                {/* <Col xs={4} className="skin-slot"></Col>
-                <Col xs={3} className="off-hand-col"> {/* Adjust to xs={3} *}
-                    <Row xs={1} className="off-hand">
-                        <Col>{/* Off-hand item *}</Col>
-                    </Row>
-                </Col> */}
-            </Row>
+        <div className="inventory-container">
+            <div className="top-row">
+                <div className="armor-slots-col">
+                    <div className="armor-slots">{/* Helmet */}</div>
+                    <div className="armor-slots">{/* Chestplate */}</div>
+                    <div className="armor-slots">{/* Leggings */}</div>
+                    <div className="armor-slots">{/* Boots */}</div>
+                </div>
+                <div className="skin-slot"></div>
+                <div className="off-hand-col">
+                    <div className="off-hand">{/* Off-hand item */}</div>
+                </div>
+            </div>
 
-            {/* Hotbar */}
-            <Row className="hotbar" xs={9}>
+            <div className="hotbar">
                 {Array.from({ length: 9 }).map((_, index) => (
-                    <Col key={index} className="inventory-slot">
+                    <div key={index} className="inventory-slot">
                         {/* Add item content here */}
-                    </Col>
+                    </div>
                 ))}
-            </Row>
+            </div>
 
-            {/* Main Inventory */}
-            {Array.from({ length: 3 }).map((_, rowIndex) => (
-                <Row key={rowIndex} xs={9}>
-                    {Array.from({ length: 9 }).map((_, colIndex) => (
-                        <Col key={colIndex} className="inventory-slot">
-                            {/* Add item content here */}
-                        </Col>
-                    ))}
-                </Row>
-            ))}
-        </Container>
+            <div className="main-inventory">
+                {Array.from({ length: 9 }).map((_, rowIndex) => (
+                    <div key={rowIndex} className="inventory-row">
+                        {Array.from({ length: 3 }).map((_, colIndex) => (
+                            <div key={colIndex} className="inventory-slot">
+                                {/* Add item content here */}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
 
