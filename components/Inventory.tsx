dd8131e69abb13e2f575961157a282084c4f0190
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-12-11 16:31:02 +0000

    Finished base style of new inventory component

--- components/Inventory.tsx
+++ components/Inventory.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Container, Row, Col, Button, Image, OverlayTrigger, Tooltip, Form } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDice, faGears, faClipboard } from '@fortawesome/free-solid-svg-icons';
 //Css
 import '../public/styles/components/Inventory.css';
 //Components
@@ -17,6 +20,48 @@ function Inventory() {
                 <div className="skin-slot"></div>
                 <div className="off-hand-col">
                     <div className="off-hand">{/* Off-hand item */}</div>
+                </div>
+                <div className="actions">
+                    <div className="top-actions">
+                        <OverlayTrigger
+                            placement="bottom"
+                            overlay={<Tooltip>Toggle Command</Tooltip>}
+                        >
+                            <Button
+                                variant="light"
+                                className="win98-button action-button"
+                            // onClick={props.toggleCommand}
+                            >
+                                <FontAwesomeIcon icon={faClipboard} size="2x" />
+                            </Button>
+                        </OverlayTrigger>
+                        <OverlayTrigger
+                            placement="bottom"
+                            overlay={<Tooltip>Settings</Tooltip>}
+                        >
+                            <Button
+                                variant="light"
+                                className="win98-button action-button"
+                                // onClick={handleModal}
+                                size="sm"
+                            >
+                                <FontAwesomeIcon icon={faGears} size="2x" />
+                            </Button>
+                        </OverlayTrigger>
+                    </div>
+                    <OverlayTrigger
+                        placement="top"
+                        overlay={<Tooltip>ReRoll Loadout</Tooltip>}
+                    >
+                        <Button
+                            id='rollBtn'
+                            variant="light"
+                            className="win98-button action-button"
+                        // onClick={props.onClick}
+                        >
+                            <FontAwesomeIcon icon={faDice} size="2x" />
+                        </Button>
+                    </OverlayTrigger>
                 </div>
             </div>
 
